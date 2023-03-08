378e44cd9f89bdfa6beccaad0c7e6e02bb1bab8f
jephthah <jephthah.mbah@outlook.com>
2023-03-08 17:48:06 +0100

    optimized the images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,19 +1,11 @@
 import VisaCardImage from "@/assets/images/Visa.png";
 import MasterCardImage from "@/assets/images/Mastercard.png";
 import ItemImage6 from "@/assets/images/item-image6.jpg";
-import ItemImage5 from "@/assets/images/item-image5.jpg";
 import ItemImage4 from "@/assets/images/item-image4.jpg";
-import ItemImage3 from "@/assets/images/item-image3.jpg";
-import ItemImage2 from "@/assets/images/item-image2.jpg";
-import ItemImage1 from "@/assets/images/item-image1.jpg";
 
 export default {
   VisaCardImage,
   MasterCardImage,
-  ItemImage1,
-  ItemImage2,
-  ItemImage3,
   ItemImage4,
-  ItemImage5,
   ItemImage6,
 };
